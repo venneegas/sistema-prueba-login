3fd51913888b9422dd7b7ac052552f547305bfad
eddvenegas <edgardhuayhuavenegas2@gmail.com>
2026-05-14 00:04:15 -0500

    Actualización VOL.XX

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: process.env.DB_HOST ? {
+    rejectUnauthorized: false
+  } : false
 });
 
 // Verificar conexión al iniciar
